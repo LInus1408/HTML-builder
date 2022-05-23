1c20a5e7227abab14e0f31d4e69bb78bfd2fca67
Hasanau Maxim <maxlinus1408@gmail.com>
2022-05-24 02:48:29 +0300

    feat: add solution 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+/* eslint-disable no-unused-vars */
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
 
+
+
+
+async function go() {
+  rimraf(path.join(__dirname, './files-copy'), function () {  
+    fs.mkdir(path.join(__dirname, './files-copy'), err => {
+      if(err) throw err; 
+      copyFiles();
+    });
+  });
+}
+
+async function copyFiles() {
+  fs.readdir(path.join(__dirname, './files'), {withFileTypes: true}, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        if(file.isFile()) {
+          fs.copyFile(path.join(__dirname, `./files/${file.name}`),path.join(__dirname, `./files-copy/${file.name}`), err => {
+            if(err) throw err;
+          });
+        }
+      });
+    }
+  });
+}
+
+
+go();
